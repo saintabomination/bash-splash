3a57222fc81efd4ff5a1f0ebb97bc4ab972cdd6c
saintabomination <saint.abomination@protonmail.com>
2022-10-15 12:47:47 +0200

    Toggling active tiles

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,16 +1,18 @@
-type Props = {
-  enabled?: boolean;
-};
+import { useState } from 'react';
 
-const Tile = ({ enabled = false }: Props): JSX.Element =>
-  (
+const Tile = (): JSX.Element => {
+  const [enabled, setEnabled] = useState<boolean>(false);
+  
+  return (
     <div
       style={{
         width: '32px',
         height: '32px',
         backgroundColor: enabled ? '#282828' : '#dedede',
       }}
+      onClick={() => setEnabled(!enabled)}
     />
   );
+}
 
 export default Tile;
