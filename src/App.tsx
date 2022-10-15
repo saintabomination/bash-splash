e0dfb5b2b4c8268db6821468f4005b1237463339
saintabomination <saint.abomination@protonmail.com>
2022-10-15 15:20:48 +0200

    Base form for resizing the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -33,13 +33,19 @@ const App = (): JSX.Element => {
   return (
     <>
       <h1>Bash Splash</h1>
+      <h2>Sizes</h2>
+      <label htmlFor="width">Width:</label><br />
+      <input type="number" name="width" /><br />
+      <label htmlFor="height">Height:</label><br />
+      <input type="number" name="height" /><br /><br />
+      <button>Resize canvas</button><br /><br />
       <Board tiles={tiles} />
       <br />
       <label htmlFor="newline">New line:</label>
       <input type="checkbox" name="newline" ref={newLineRef} />
       <button onClick={handleGeneration}>Generate</button>
-      <button onClick={handleReset}>Reset</button><br /><br />
-      <label htmlFor="output">Output:</label><br />
+      <button onClick={handleReset}>Reset</button>
+      <h2>Output</h2>
       <textarea
         name="output"
         cols={48}
