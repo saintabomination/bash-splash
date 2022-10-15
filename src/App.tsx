a239f762f7896e43d71f26c0d54a4f1dd15e4a68
saintabomination <saint.abomination@protonmail.com>
2022-10-15 15:26:26 +0200

    Resizing the canvas functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import Board from './components/Board';
 import { generateBoardBase, resetBoard } from './redux/slices/boardSlice';
 import { generateScript } from './functions/generateScript';
 
+import type { FormEvent } from 'react';
+
 import type { RootState } from './redux/store';
 
 const App = (): JSX.Element => {
+  const widthRef = useRef<HTMLInputElement | null>(null);
+  const heightRef = useRef<HTMLInputElement | null>(null);
   const newLineRef = useRef<HTMLInputElement | null>(null);
   const outputRef = useRef<HTMLTextAreaElement | null>(null);
   const { tiles } = useSelector((state: RootState) => state.board);
@@ -19,6 +23,17 @@ const App = (): JSX.Element => {
       height: 8,
     }));
   }, [dispatch]);
+
+  const handleResize = (e: FormEvent) => {
+    e.preventDefault();
+    if (!widthRef.current || !heightRef.current) return;
+    if (Number(widthRef.current.value) <= 0 || Number(heightRef.current.value) <= 0) return;
+
+    dispatch(generateBoardBase({
+      width: Number(widthRef.current.value),
+      height: Number(heightRef.current.value),
+    }));
+  }
 
   const handleGeneration = () => {
     if (!outputRef.current) return;
@@ -34,11 +49,13 @@ const App = (): JSX.Element => {
     <>
       <h1>Bash Splash</h1>
       <h2>Sizes</h2>
-      <label htmlFor="width">Width:</label><br />
-      <input type="number" name="width" /><br />
-      <label htmlFor="height">Height:</label><br />
-      <input type="number" name="height" /><br /><br />
-      <button>Resize canvas</button><br /><br />
+      <form onSubmit={e => handleResize(e)}>
+        <label htmlFor="width">Width:</label><br />
+        <input type="number" name="width" ref={widthRef} required /><br />
+        <label htmlFor="height">Height:</label><br />
+        <input type="number" name="height" ref={heightRef} required /><br /><br />
+        <button>Resize canvas</button><br /><br />
+      </form>
       <Board tiles={tiles} />
       <br />
       <label htmlFor="newline">New line:</label>
