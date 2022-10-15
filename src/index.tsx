b3dc793175c04709e49e43f72d9894eedda6f1b2
saintabomination <saint.abomination@protonmail.com>
2022-10-15 21:11:20 +0200

    CSS Reset

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { Provider as ReduxProvider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
+import './styles/reset.scss';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
